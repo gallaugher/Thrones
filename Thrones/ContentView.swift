94c44424f8a0adc54608f5f57ea38d771b638ec9
gallaugher <john.gallaugher@bc.edu>
2024-11-10 18:41:34 -0500

    Initial Commit

---
+++ Thrones/ContentView.swift
@@ -0,0 +1,21 @@
+// File: ContentView.swift Project: Thrones
+// Created by: Prof. John Gallaugher on 11/10/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thrones/ThronesApp.swift
@@ -0,0 +1,14 @@
+// File: ThronesApp.swift Project: Thrones
+// Created by: Prof. John Gallaugher on 11/10/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct ThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
